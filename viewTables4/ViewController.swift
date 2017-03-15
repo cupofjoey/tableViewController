54ae2c110a7440501dd4e67c69421bf30689c690
Joe Hill <joe@jkh3.com>
2017-03-15 00:06:50 -0400

    First attempt at Table View Controller in Swift

---
+++ viewTables4/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  viewTables4
+//
+//  Created by BishopHill on 3/14/17.
+//  Copyright © 2017 Dumb Unicorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+
+   
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 50
+        
+    }
+    
+    
+   
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = String(indexPath.row + 1)
+        
+        return cell
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
